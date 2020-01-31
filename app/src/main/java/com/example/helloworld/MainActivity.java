ee1404500699eded34eeef266ace19a7d57a7887
Sofia Khan <sakhan8@wisc.edu>
2020-01-31 15:26:44 -0600

    Here goes again

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    //blah blah blah
+    //blah blah blah blah
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
